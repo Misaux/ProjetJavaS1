049fb786d7fd4caacc3b6e4e7086d22039f68c0a
OscarBurdin <oscar.burdin@edu.ece.fr>
2020-10-22 15:08:14 +0200

    test git 2

--- src/Cours/Main.java
+++ src/Cours/Main.java
@@ -5,6 +5,8 @@ import Cours.Cours;
 public class Main {
     public static void main(String[] args) {
         Cours test = new Cours(Cours.Campus.LYON, Cours.Batiment.C, 402, Cours.TypeCourse.PRESENTIEL, Cours.Course.INFO, Cours.Groupe.A);
+        Cours test1= new Cours(Cours.Campus.PARIS, Cours.Batiment.D,69, Cours.TypeCourse.PRESENTIEL, Cours.Course.PHYSIQUE,Cours.Groupe.C);
         System.out.println(test.toString());
+        System.out.println(test1.toString());
     }
 }
